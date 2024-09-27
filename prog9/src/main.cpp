1ca16f39775b58424d8aaff22695f5df63b88684
jorgesalhani <jorge.salhani@usp.br>
2024-09-27 00:08:19 -0300

    done prog9. last cases in doubt

--- prog9/src/main.cpp
+++ prog9/src/main.cpp
@@ -26,18 +26,14 @@ long int calcularMenorCusto(vecLongInt custos, long int i) {
 
   if (memo[i] != -1) return memo[i];
 
-  if (i == 0) return custos[i];
+  if (i == 0) return memo[i] = 0;
+  long int ci1 = abs(custos[i-1] - custos[i]);
   if (i == 1) 
     return memo[i] = abs(custos[i-1] - custos[i]);
   
-  long int ci1 = abs(custos[i-1] - custos[i]);
   long int ci2 = abs(custos[i-2] - custos[i]);
-  long int prox = ci1 <= ci2 ? i-1 : i-2;
-  memo[i] = min(ci1, ci2) + min(calcularMenorCusto(custos, prox), calcularMenorCusto(custos, i-1));
 
-  for (auto &x : memo) cout << x << ", ";
-  cout << endl;
-
+  memo[i] = min(ci2 + calcularMenorCusto(custos, i-2), ci1 + calcularMenorCusto(custos, i-1));
   return memo[i];
 }
 
@@ -48,10 +44,6 @@ int main(void) {
   cin >> N;
 
   vecLongInt custos = obterEntradas(N);
-
-  for (auto &x : custos) cout << x << " ";
-  cout << endl;
-
   cout << calcularMenorCusto(custos, N-1) << endl;
 
   return 0;
