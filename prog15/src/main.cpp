c5a5cb77401e453046e4a6bd80c2127a5310d052
jorgesalhani <jorge.salhani@usp.br>
2024-10-26 21:56:18 -0300

    prog15

---
+++ prog15/src/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+#define N_MAX_PRIMOS 50
+
+using namespace std;
+
+typedef vector<long long int> vi;
+typedef vector<vi> v2i;
+
+v2i obterMatrizEntrada(int N, int M) {
+  v2i matrizEntrada(N, vi(M, -1));
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      int val;
+      cin >> val;
+      matrizEntrada[i][j] = val;
+    }
+  }
+  return matrizEntrada;
+}
+
+bitset<N_MAX_PRIMOS> montarCrivoPrimos(long long int maxPrimos){
+  bitset<N_MAX_PRIMOS> crivo;
+	crivo.set();
+
+	crivo[0] = crivo[1] = 0;
+
+	for (long long int i = 2; i <= maxPrimos + 1; ++i){
+		if (crivo[i]){
+			for (long long int j = i*i; j <= maxPrimos + 1; j += i)
+				crivo[j]=0;
+		}
+	}
+
+  return crivo;
+}
+
+void printMatriz(v2i matrizEntrada) {
+  for (auto &x : matrizEntrada) {
+    for (auto &y : x) cout << y << " ";
+    cout << endl;
+  }
+}
+
+int obterMenorNumeroOperacoes(v2i matrizEntrada, bitset<N_MAX_PRIMOS> crivo) {
+
+  int M = matrizEntrada.size();
+  int N = matrizEntrada[0].size();
+
+  v2i matrizNumeroOps(M, vi(N, 0));
+  vi totalOpsPorLinha;
+  for (int i = 0; i < M; i++) {
+    long long int totalOps = 0;
+    for (int j = 0; j < N; j++) {
+      long long int original = matrizEntrada[i][j];
+      while (!crivo[original]) {
+        original++;
+        matrizNumeroOps[i][j] += 1;
+        totalOps++;
+      }
+    }
+    totalOpsPorLinha.push_back(totalOps);
+  }
+  // for (auto &x : totalOpsPorLinha) cout << x << " ";
+  // cout << endl;
+
+  long long int minLinha = min_element(totalOpsPorLinha.begin(), totalOpsPorLinha.end())[0];
+
+  vi totalOpsPorColuna;
+  for (int i = 0; i < N; i++) {
+    long long int totalOps = 0;
+    for (int j = 0; j < M; j++) {
+      totalOps += matrizNumeroOps[j][i];
+    }
+    totalOpsPorColuna.push_back(totalOps);
+  }
+
+  long long int minColuna = min_element(totalOpsPorColuna.begin(), totalOpsPorColuna.end())[0];
+
+  return min(minLinha, minColuna);
+}
+
+int main(void) {
+
+  int N, M;
+  cin >> N;
+  cin >> M;
+
+  bitset<N_MAX_PRIMOS> crivo = montarCrivoPrimos(N_MAX_PRIMOS);
+
+  v2i matrizEntrada = obterMatrizEntrada(N, M);
+
+  // printMatriz(matrizEntrada);
+  // cout << endl;
+  cout << obterMenorNumeroOperacoes(matrizEntrada, crivo);
+
+  return 0;
+}
---
+++ prog15/testCases/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 26/10/2024 16:58:32 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: TNum1
